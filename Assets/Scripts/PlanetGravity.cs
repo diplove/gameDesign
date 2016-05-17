a58269bd7ef790e4783cfd9494fa6fcda20469d7
afonfarski <afonfarski@gmail.com>
2016-05-18 00:19:24 +1000

    + Movement + Rotation + Planet Orbit + Planet Dead Zone

---
+++ Assets/Scripts/PlanetGravity.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlanetGravity : MonoBehaviour {
+
+    public float mass;
+    public float gravConstant;
+
+    [SerializeField]
+    private Collider2D gravDeadZone;
+    [SerializeField]
+    private Collider2D gravOrbit;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void FixedUpdate()
+    {
+
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (gravDeadZone.IsTouching(other))
+        {
+            Vector3 diff = transform.position - other.transform.position;
+            Vector3 direction = diff.normalized;
+            Rigidbody2D otherRb = other.gameObject.GetComponent<Rigidbody2D>();
+
+            float gravForce = (mass * otherRb.mass * gravConstant) / diff.sqrMagnitude;
+
+            otherRb.AddForce(direction * gravForce);
+        }
+        if (gravOrbit.IsTouching(other))
+        {
+            other.gameObject.transform.RotateAround(transform.position, Vector3.forward, 5 * Time.deltaTime);
+        }
+    }
+}
