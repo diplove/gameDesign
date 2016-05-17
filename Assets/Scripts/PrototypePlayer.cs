4686ba04da4804a329f7f6aa0a164233c77bfaf6
diplove <diplove.stha@gmail.com>
2016-05-17 18:29:06 +1000

    Add files via upload

---
+++ Assets/Scripts/PrototypePlayer.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PrototypePlayer : MonoBehaviour {
+	//public rigidbody2D rb = GetComponent<Rigidbody2D> (); //apparently depreciated
+	public float maxSpeed;
+	public float turnRate;
+	public float acelRate;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		/*
+		if (Input.GetKey(KeyCode.UpArrow)) {
+			Accelerate ();
+		}
+		*/
+
+	}
+
+	void OnCollisionEnter2D (Collision2D coll) {
+		//if (coll.gameObject.tag == "PlanetGravity") {
+		//
+		//}
+	}
+
+	/*
+	void Accelerate () {
+		rb.AddForce (acelRate);
+	}
+	*/
+}
---
+++ Assets/Scripts/followPlayer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class followPlayer : MonoBehaviour {
+	public Transform target;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3 (target.position.x, target.position.y, -10);
+	}
+}
