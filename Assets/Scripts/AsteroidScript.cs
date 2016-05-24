13509d92022f0498c0cfbfc57c0dd69fb4af259a
diplove <diplove.stha@gmail.com>
2016-05-24 13:07:47 +1000

    Added some asteroids to revolve around the scene..

---
+++ Assets/Scripts/AsteroidScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AsteroidScript : MonoBehaviour
+{
+    public float MinTorque = -100f;
+    public float MaxTorque = 100f;
+    public float MinForce = 20f;
+    public float MaxForce = 40f;
+
+	void Start ()
+    {
+        Rigidbody2D rb2d = GetComponent<Rigidbody2D>();
+
+        float magnitude = Random.Range(MinForce, MaxForce);
+        float x = Random.Range(-1f, 1f);
+        float y = Random.Range(-1f, 1f);
+
+        rb2d.AddForce(magnitude * new Vector2(x, y));
+
+        float torque = Random.Range(MinTorque, MaxTorque);
+        rb2d.AddTorque(torque);
+	}
+	
+	void Update ()
+    {
+
+	}
+}
