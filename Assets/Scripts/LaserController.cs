3e42bfd7eb567f154701482b7186669059cc3443
Afonfarksi <adam.fonfarski@student.uts.edu.ay>
2016-05-28 13:02:20 +1000

    + Changed laser (Please test)

--- Assets/Scripts/LaserController.cs
+++ Assets/Scripts/LaserController.cs
@@ -10,12 +10,45 @@ public class LaserController : MonoBehaviour {
 
 	public GameObject laser;
 
-	public float offset; //To get the Laser to project from the vessel
+    public GameObject laserParticles;
+
+    public float laserMaxDistance;
+
+    private float damage;
+
+
+	//public float offset; //To get the Laser to project from the vessel
 
 	// Use this for initialization
 	void Start () {
+        laser = Instantiate(laser);
+        laserParticles = Instantiate(laserParticles);
+        laserParticles.SetActive(false);
 		laser.SetActive (false);
+        UpdateDamage();
 	}
+
+    void FixedUpdate() {
+
+        if (fire == true) {
+            laser.GetComponent<LineRenderer>().SetPosition(0, weaponHardpoint.transform.position);
+            RaycastHit2D hit = Physics2D.Raycast(weaponHardpoint.transform.position, transform.up, laserMaxDistance);
+
+            if(hit.collider != null) {
+                laser.GetComponent<LineRenderer>().SetPosition(1, hit.point);
+                laserParticles.transform.position = hit.point;
+                laserParticles.SetActive(true);
+            } else {
+                laser.GetComponent<LineRenderer>().SetPosition(1, weaponHardpoint.transform.position + (transform.up * laserMaxDistance));
+                laserParticles.SetActive(false);
+            }
+            
+           laser.SetActive(true);
+        } else {
+            laser.SetActive(false);
+            laserParticles.SetActive(false);
+        }
+    }
 
 	public void activateLaser() {
 		fire = true;
@@ -25,14 +58,22 @@ public class LaserController : MonoBehaviour {
 		fire = false;
 	}
 
-	// Update is called once per frame
-	void Update () {
-		if (fire == true) {
-			laser.transform.position = new Vector3 (weaponHardpoint.transform.position.x, weaponHardpoint.transform.position.y, -1);
+    void UpdateDamage() {
+        damage = FindObjectOfType<PrototypePlayer>().primDamage;
+    }
+
+    public void setDamage(float damageAmount) {
+        damage = damageAmount;
+    }
+
+    // Update is called once per frame
+    void Update () {
+		/*if (fire == true) {
+            laser.SetActive(true);
+            laser.transform.position = new Vector3 (weaponHardpoint.transform.position.x, weaponHardpoint.transform.position.y, -1);
 			laser.transform.rotation = weaponHardpoint.transform.rotation;
-			laser.SetActive (true);
 		} else {
 			laser.SetActive (false);
-		}
+		}*/
 	}
 }
