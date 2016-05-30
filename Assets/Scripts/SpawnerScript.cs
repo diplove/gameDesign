266c339542f65ad85dcd67dc565cd09ce316d672
diplove <diplove.stha@gmail.com>
2016-05-30 21:56:32 +1000

    +Created Spawner GameObject for Asteroids +Made spawnLocations as Spawner's child gameobject +Added SpawnerScript to clone asteroid at spawnLocations

---
+++ Assets/Scripts/SpawnerScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnerScript : MonoBehaviour {
+
+    public Transform[] spawnLocations;
+    public GameObject[] whatToSpawnPrefab;
+    public GameObject[] whatToSpawnClone;
+
+    void Start()
+    {
+        //InvokeRepeating("spawnAsteroids", 0, 2.0f);
+        spawnAsteroids();
+    }
+
+    void spawnAsteroids()
+    {
+        for (int i=0; i<spawnLocations.Length; i++)
+        {
+            whatToSpawnClone[0] = Instantiate(whatToSpawnPrefab[0], spawnLocations[i].transform.position, Quaternion.identity) as GameObject;
+        }
+    }
+
+    void Update()
+    {
+
+    }
+}
